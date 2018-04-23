fe234e2b8a2509c136f6dbc6de406adb09062cdd
Mitch Dorrestijn <mitchdorrestijn@kpnmail.nl>
2018-04-23 16:41:55 +0200

    Added footer for the front-end

---
+++ src/main/components/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Col } from 'reactstrap';
+
+import '../css/footer.css';
+
+const Footer = (props) => {
+	return (
+    <footer>
+		    <small>Heeft u vragen? Bel ons:</small>
+      <span>020 - 123 45 67</span>
+    </footer>
+	);
+}
+
+export default Footer;
--- src/main/container/TablePage.js
+++ src/main/container/TablePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
 import TextContent from '../components/TextContent';
+import Footer from '../components/Footer';
 import { Container, Row, Col } from 'reactstrap';
 
 export default class TablePage extends Component {
@@ -13,6 +14,8 @@ export default class TablePage extends Component {
             <TextContent title="Betalingen">
               Cookie tiramisu bonbon dragée danish icing danish I love. Lemon drops halvah tiramisu tootsie roll cheesecake. Candy canes candy canes jelly beans I love. Tootsie roll I love cheesecake gingerbread dragée cupcake dragée marzipan cupcake.
             </TextContent>
+            [tabel komt hier]
+            <Footer />
           </Col>
         </Row>
       </Container>
