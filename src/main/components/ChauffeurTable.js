e1d39de6600390f79e95fc900de1fae8492797f4
Mitch Dorrestijn <M.Dorrestijn1@student.han.nl>
2018-05-14 15:34:29 +0200

    Added setup for chauffeurs table page on the front-end

---
+++ src/main/components/ChauffeurTable.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class ChauffeurTable extends React.Component {
+  render(){
+    return(
+      <div>Chauffeur table komt hier</div>
+    );
+  }
+}
--- src/main/routers/AppRouter.js
+++ src/main/routers/AppRouter.js
@@ -25,23 +25,20 @@ const routes = {
   readZorginstelling: '/read/zorginstelling',
   createZorginstelling: '/create/zorginstelling',
   updateZorginstelling: '/update/zorginstelling',
-  error: '/error'
+  error: '/error',
+  readChauffeur: '/read/chauffeur',
 }
 
 const AppRouter = () => {
   return (
 		<BrowserRouter>
 	    <Switch>
-	      <Route exact path={routes.login}
-					render={(props) => (<LoginPage {...props} routes={routes}/>)}/>
-	      <Route exact path={routes.readZorginstelling}
-					render={(props) => (<ReadPage {...props} routes={routes}/>)}/>
-	      <Route exact path={routes.createZorginstelling}
-					render={(props) => (<CreatePage {...props} routes={routes}/>)}/>
-	      <Route exact path={`${routes.updateZorginstelling}/:id`}
-				render={(props) => (<UpdatePage {...props} routes={routes}/>)}/>
-        <Route exact path={`${routes.error}`}
-        render={(props) => (<ErrorPage {...props} routes={routes}/>)}/>
+	      <Route exact path={routes.login} render={(props) => (<LoginPage {...props} routes={routes}/>)} />
+        <Route exact path={routes.readZorginstelling} render={(props) => (<ReadPage {...props} routes={routes}/>)} />
+        <Route exact path={routes.readChauffeur} render={(props) => (<ReadPage {...props} routes={routes}/>)} />
+	      <Route exact path={routes.createZorginstelling} render={(props) => (<CreatePage {...props} routes={routes}/>)} />
+	      <Route exact path={`${routes.updateZorginstelling}/:id`} render={(props) => (<UpdatePage {...props}  routes={routes} />)}/>
+        <Route exact path={`${routes.error}`} render={(props) => (<ErrorPage {...props} routes={routes}/>)}/>
 	    </Switch>
   	</BrowserRouter>
 	);
