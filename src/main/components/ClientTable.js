ef2b62483363ad901ab0c14f4655ae6988165da5
meesVDsloot <MR.vanderSloot@student.han.nl>
2018-05-15 14:52:41 +0200

    Policy error, test to see if it works on a different workstation

---
+++ src/main/components/ClientTable.js
@@ -0,0 +1,145 @@
+/**
+ * React related imports
+ */
+import React from 'react';
+import {Button} from 'reactstrap'
+import {CSVLink} from 'react-csv';
+import axios from 'axios';
+import {BootstrapTable, TableHeaderColumn, SearchField} from 'react-bootstrap-table';
+import config from '../config';
+
+/**
+ * Style related imports
+ */
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import '../css/table.css';
+
+/**
+ * This class takes care of rendering the client table, from here other actions can be taken
+ */
+export default class ClientTable extends React.Component {
+  componentDidMount() {
+    axios.get(`${config.url}/client/clients`).then((res) => {
+      console.log(res);
+      this.setState({data: res.data});
+    }).catch((err) => {
+      return this.props.history.push(this.props.routes.error);
+    });
+  }
+
+  /**
+   * Setups all the data.
+   * @constructor
+   * @param {props} props - The given properties by the superclass
+   */
+  constructor(props) {
+    super(props);
+    this.columns = [
+      {
+        name: 'warning',
+        display: 'WARNING'
+      }, {
+        name: 'name',
+        display: 'Naam cliënt'
+      }, {
+        name: 'PKB',
+        display: 'PKB cliënt'
+      }, {
+        name: 'madeKilometers',
+        display: 'Gemaakte km's''
+      }, {
+        name: 'toPay',
+        display: 'Te betalen door cliënt'
+      }
+    ];
+    this.state = {
+      sortColumnName: undefined,
+      sortOrder: undefined,
+      disableButtons: true,
+      data: []
+    };
+  }
+
+  /**
+   * Renders the columns sorted.
+   * @param {string} sortColumnName - Name of the column that is going to be sorted
+   * @param {string} sortOrder - Can be either desc or asc
+   */
+  renderSortedColumn = (sortColumnName, sortOrder) => {
+    this.setState({sortColumnName: sortColumnName, sortOrder: sortOrder});
+  };
+
+  /**
+   * Renders the search field above the table
+   */
+  renderSearchField = () => {
+    return (<SearchField className='searchfield' placeholder='Type om te zoeken'/>);
+  };
+
+  /**
+   * Renders the buttons above the table
+   */
+  renderButtons = () => {
+    return (<div>
+      <Button color="primary" className='crud-btn'>Toevoegen</Button>
+      <Button disabled={this.state.disableButtons} color="primary" className='crud-btn'>Bewerken</Button>
+    </div>);
+  };
+
+  /**
+   * Renders the view for the user
+   */
+  render() {
+    const tableOptions = {
+      sortColumnName: this.state.sortName,
+      sortOrder: this.state.sortOrder,
+      renderSortedColumn: this.renderSortedColumn,
+      noDataText: 'Geen resultaten gevonden',
+      searchField: this.renderSearchField,
+      btnGroup: this.renderButtons
+    };
+
+    return (<div>
+      <BootstrapTable search={true} data={this.state.data} options={tableOptions} selectRow={{
+          mode: 'radio',
+          onSelect: this.onSelect
+        }} ref='table'>
+
+        <TableHeaderColumn hidden={true} dataField="id" isKey={true}>
+          ID
+        </TableHeaderColumn>
+
+        <TableHeaderColumn width="200" dataField="warning" dataSort={true}>
+          Warning
+        </TableHeaderColumn>
+
+        <TableHeaderColumn width="100" dataField="name" dataSort={true}>
+          Naam
+        </TableHeaderColumn>
+
+        <TableHeaderColumn width="130" dataField="PKB" dataSort={true}>
+          PKB
+          <br/>
+          cliënt &#x2195;
+        </TableHeaderColumn>
+
+        <TableHeaderColumn width="130" dataField="madeKilometers" dataSort={true}>
+          Gemaakte
+          <br/>
+          km's' &#x2195;
+        </TableHeaderColumn>
+
+        <TableHeaderColumn width="130" dataField="toPay" dataSort={true}>
+          Te betalen
+          <br/>
+          door cliënt &#x2195;
+        </TableHeaderColumn>
+
+      </BootstrapTable>
+
+      <CSVLink data={this.state.data} filename={"clienten_overview"} className="btn btn-primary crud-btn" target="">
+        Export als CSV
+      </CSVLink>
+    </div>);
+  };
+}
--- src/main/container/ReadPage.js
+++ src/main/container/ReadPage.js
@@ -15,6 +15,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 import ReadZorginstelling from '../views/zorginstelling/ReadZorginstelling';
 import ReadChauffeur from '../views/chauffeur/ReadChauffeur';
+import ReadClient from '../views/client/ReadClient';
 
 /**
  * This component renders the read (overview) page for the elements based on the URL
@@ -30,6 +31,9 @@ const ReadPage = (props) => {
 						}
 						{props.match.url === props.routes.readChauffeur &&
 							<ReadChauffeur routes={props.routes} history={props.history} />
+						}
+						{props.match.url === props.routes.readClienten &&
+							<ReadClient routes={props.routes} history={props.history} />
 						}
           <Footer />
         </Col>
