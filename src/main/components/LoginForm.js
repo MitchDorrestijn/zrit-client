55104e4829d92e8e69b4b10732ba5dcec53314d1
Mitch Dorrestijn <M.Dorrestijn1@student.han.nl>
2018-05-18 11:43:01 +0200

    Added name field to the login form fields

--- src/main/components/LoginForm.js
+++ src/main/components/LoginForm.js
@@ -28,11 +28,11 @@ const LoginForm = () => {
           <Label for="inputUserName" className="float-left">
             <b>Gebruikersnaam</b>
           </Label>
-          <Input type="text" id="inputUserName" className="mb-3 " placeholder="Gebruikersnaam" autoFocus />
+          <Input name="username" type="text" id="inputUserName" className="mb-3 " placeholder="Gebruikersnaam" autoFocus />
           <Label for="inputPassword" className="float-left">
             <b>Wachtwoord</b>
           </Label>
-          <Input type="password" id="inputPassword" className="mb-4" placeholder="Wachtwoord"/>
+          <Input name="password" type="password" id="inputPassword" className="mb-4" placeholder="Wachtwoord"/>
           <Button color="primary" className="crud-btn" size="lg" type="submit">Inloggen</Button>
         </Form>
       </div>
