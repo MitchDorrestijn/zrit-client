1ec4d87a80ec4f65559242232110d58a99866ee9
Mitch Dorrestijn <M.Dorrestijn1@student.han.nl>
2018-06-01 11:24:05 +0200

    Created table for betalingen

--- src/main/components/BetalingenTable.js
+++ src/main/components/BetalingenTable.js
@@ -1,11 +1,108 @@
+/**
+ * React related imports
+ */
 import React from 'react';
+import {Button} from 'reactstrap'
+import {CSVLink} from 'react-csv';
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import {renderSearchField, renderSortedColumn} from '../global/Methods';
 
+/**
+ * Style related imports
+ */
+import '../../../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+import '../css/table.css';
+
+/**
+ * This class takes care of rendering the betalingen table, from here other actions can be taken
+ */
 export default class BetalingenTable extends React.Component {
-  render(){
-    return(
+
+  /**
+   * Setups all the data.
+   * @constructor
+   * @param {props} props - The given properties by the superclass
+   */
+  constructor(props) {
+    super(props);
+    this.columns = [
+      {
+        name: 'chauffeur',
+        display: 'Chauffeur'
+      },
+      {
+        name: 'overmaken_naar',
+        display: 'Overmaken naar'
+      },
+      {
+        name: 'bedrag',
+        display: 'Bedrag'
+      },
+      {
+        name: 'betalingsKenmerk',
+        display: 'Betalingskenmerk'
+      },
+      {
+        name: 'periode_betaling',
+        display: 'Periode betaling'
+      },
+    ];
+    this.state = {
+      sortColumnName: undefined,
+      sortOrder: undefined,
+      disableButtons: true,
+      data: []
+    };
+  };
+
+  /**
+   * Renders the view for the user
+   */
+  render() {
+    const tableOptions = {
+      sortColumnName: this.state.sortName,
+      sortOrder: this.state.sortOrder,
+      renderSortedColumn: renderSortedColumn,
+      noDataText: 'Geen resultaten gevonden',
+      searchField: renderSearchField
+    };
+
+    return (
       <div>
-        <h1>BetalingenTable</h1>
+        <BootstrapTable search={true} data={this.state.data} options={tableOptions} selectRow={{
+            mode: 'radio',
+            onSelect: this.onSelect
+          }} ref='table'>
+
+          <TableHeaderColumn hidden={true} dataField="id" isKey={true}>
+            ID
+          </TableHeaderColumn>
+
+          <TableHeaderColumn dataField="chauffeur" dataSort={true}>
+            Chauffeur &#x2195;
+          </TableHeaderColumn>
+
+          <TableHeaderColumn dataField="overmaken_naar" dataSort={true}>
+            Overmaken naar &#x2195;
+          </TableHeaderColumn>
+
+          <TableHeaderColumn dataField="bedrag" dataSort={true}>
+            Bedrag &#x2195;
+          </TableHeaderColumn>
+
+          <TableHeaderColumn dataField="betalingsKenmerk" dataSort={true}>
+            Betalingskenmerk &#x2195;
+          </TableHeaderColumn>
+
+          <TableHeaderColumn dataField="periode_betaling" dataSort={true}>
+            Periode betaling &#x2195;
+          </TableHeaderColumn>
+        </BootstrapTable>
+
+        <CSVLink data={this.state.data} filename={"betalingen_overview"} className="btn btn-primary crud-btn" target="">
+          Export als CSV
+        </CSVLink>
       </div>
     );
-  }
+  };
 }
