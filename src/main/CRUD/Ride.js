3709e83429d3c7a19902dab4573b1e35c8ee94b5
meesVDsloot <MR.vanderSloot@student.han.nl>
2018-06-05 16:05:48 +0200

    added java docs

--- src/main/CRUD/Ride.js
+++ src/main/CRUD/Ride.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 import config from '../config';
 
+/**
+  * Sends a POST request to add a new ride
+  * @param {data} data - the object that holds the data for the new ride
+**/
 export const createRide = (data) => {
   return axios.post(`${config.url}/ride/create`, data);
 }
 
+/**
+  * Sends a GET to get all rides
+  * @param {data} data - the data that gets used for the GET request
+**/
 export const getAllRides = (data) => {
   return axios.get(`${config.url}/ride/getAllRides`);
 }
